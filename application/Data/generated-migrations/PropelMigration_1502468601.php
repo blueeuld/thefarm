e80ce28f1dbe0a946c42534382fc9860b8ba65c8
Marvin Lara <marvin@liquamedia.com>
2017-08-12 00:25:11 +0800

    Rebuild models

---
+++ application/Data/generated-migrations/PropelMigration_1502468601.php
@@ -0,0 +1,88 @@
+<?php
+
+use Propel\Generator\Manager\MigrationManager;
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1502468601.
+ * Generated on 2017-08-11 16:23:21 by marvin
+ */
+class PropelMigration_1502468601
+{
+    public $comment = '';
+
+    public function preUp(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        return array (
+  'default' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP INDEX `fi_tact_fk2` ON `tf_user_work_plan_time`;
+
+CREATE INDEX `user_work_plan_time_contact_fk` ON `tf_user_work_plan_time` (`contact_id`);
+
+ALTER TABLE `tf_user_work_plan_time` ADD CONSTRAINT `user_work_plan_time_contact_fk`
+    FOREIGN KEY (`contact_id`)
+    REFERENCES `tf_contacts` (`contact_id`);
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        return array (
+  'default' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `tf_user_work_plan_time` DROP FOREIGN KEY `user_work_plan_time_contact_fk`;
+
+DROP INDEX `user_work_plan_time_contact_fk` ON `tf_user_work_plan_time`;
+
+CREATE INDEX `fi_tact_fk2` ON `tf_user_work_plan_time` (`contact_id`);
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+}
